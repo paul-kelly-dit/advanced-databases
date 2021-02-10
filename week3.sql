a936e125db53dd84e8eec8571e1a4d39a4254d65
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2021-02-10 19:10:01 +0000

    updated varchar to 400

--- week3.sql
+++ week3.sql
@@ -1,7 +1,7 @@
 create table if not exists FILM (
     FILM_ID INT  not null primary key,
     LENGTH INT not null,
-    TITLE VARCHAR(50) not null,
+    TITLE VARCHAR(400) not null,
     RENTAL_DURATION INT not null
 );
 
