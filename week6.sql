b103df2fd9c7b4121977934bdfbd796abd937052
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2021-03-24 17:43:50 +0000

    Create week6.sql

---
+++ week6.sql
@@ -0,0 +1,15 @@
+-- Output each state and their total sales_amount
+SELECT ___.___, ___(___)
+FROM ___
+	-- Join to get book information
+    JOIN ___ on ___.___ = ___.___
+	-- Join to get store information
+    JOIN ___ on ___.___ = ___.___
+-- Get all books with in the novel genre
+WHERE
+    ___.___ = 'novel'
+-- Group results by state
+GROUP BY
+    ___.___;
+
+    
