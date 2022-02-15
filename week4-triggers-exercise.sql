158ef70274cbdf85ed6973733f642e026fc90f80
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2022-02-15 19:51:03 +0000

    Create week4-triggers-exercise.sql

---
+++ week4-triggers-exercise.sql
@@ -0,0 +1,8 @@
+CREATE TABLE student_grade (
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    names varchar(200),
+    CA_scores int(11),
+    test_scores int(11),
+    exam_scores int(11),
+    total_scores int(11)
+);
