83113290479a1bf2fa37fc597731d8aef6829c18
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2022-02-16 20:21:49 +0000

    solution to question 2

--- week4-triggers-lab-exercises.sql
+++ week4-triggers-lab-exercises.sql
@@ -19,4 +19,21 @@ VALUES (2, "PaulKelly", NOW(), 600, 'n', 10);
 # Should see an error thrown when you try to execute this.
 
 
+# Question 2 - cannot change country code
+
+create TRIGGER cannot_change_country_code
+BEFORE UPDATE ON shops
+for each row
+    begin
+        IF (OLD.country <> NEW.country) THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot change country code';
+        END IF;
+    end;
+
+insert into shops(shop_id, name, address, country) values (1, 'shop 2', 'dublin', 'irl');
+# should error
+update shops set country = 'uk' where shop_id=1;
+# should not error as we are not changing country code
+update shops set address = 'carlow' where shop_id=1;
+
 
