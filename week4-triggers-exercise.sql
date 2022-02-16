57aabb06f669889b20f6aa3f4bf014cba89ff23b
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2022-02-16 08:09:48 +0000

    Update week4-triggers-exercise.sql

--- week4-triggers-exercise.sql
+++ week4-triggers-exercise.sql
@@ -6,3 +6,14 @@ CREATE TABLE student_grade (
     exam_scores int(11),
     total_scores int(11)
 );
+
+TODO - Do your triggers before the next step
+
+INSERT INTO student_grade
+(id,names,CA_scores ,test_scores,exam_scores) 
+VALUES
+(1, "John Miller", 20, 20, 40),
+(2, "Jean Miller", 12, 20, 50),
+(3, "Sean Miller", 20, 15, 30); 
+ 
+ SELECT * FROM student_grade
